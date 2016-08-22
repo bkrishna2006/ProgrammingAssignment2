f8ff3b11f29ec6cd32bedeb53cc6a03da6fefe0e
bkrishna2006 <bkrishna2006@gmail.com>
2016-08-23 04:06:16 +0530

    Updated with a test function
    Added comments to each function module and added a function module to test the first two functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   ivar <- NULL
   set <- function(yvar) {
-    x <<- yvar
-    ivar <<- NULL
+  x <<- yvar
+  ivar <<- NULL
   }
   get <- function() x
   setinverse <- function(inv) ivar <<- inv
@@ -18,8 +15,10 @@ makeCacheMatrix <- function(x = matrix()) {
     setinverse = setinverse,
     getinverse = getinverse
   )
-  
 }
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+#above.If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   ivar <- x$getinverse()
@@ -31,4 +30,18 @@ cacheSolve <- function(x, ...) {
   ivar <- solve(mvar, ...)
   x$setinverse(ivar)
   ivar
-  }
+}
+
+# function to test makeCacheMatrix and cacheSolve functions on a nXn matrix mat1
+set.seed(1110201)
+r = rnorm(1000000)
+mat1 = matrix(r, nrow=1000, ncol=1000)
+temp = makeCacheMatrix(mat1)
+start.time = Sys.time()
+cacheSolve(temp)
+dur = Sys.time() - start.time
+print(dur)
+start.time = Sys.time()
+cacheSolve(temp)
+dur = Sys.time() - start.time
+print(dur)
